cf0cae9484c9694106c48f8a57821c53f23062a4
MiguelAngel <mmontijanosempere@gmail.com>
2026-04-12 13:43:00 +0200

    Primer commit: Subida del archivo coxnnet_pytorch donde se creará el nuevo modelo y .gitignore

---
+++ coxnnet_pytorch.py
@@ -0,0 +1,15 @@
+# Este es un programa para ejecutar la extensión de red neuronal de la regresión de Cox
+#Clases:
+#   CoxRegression - la capa de salida de la regresión de Cox
+#   HiddenLayer - capas ocultas entre la entrada y la salida
+#   CoxMlp - clase contenedora para la capa de salida y las capas ocultas
+
+#Funciones: 
+#   createSharedDataset - función auxiliar para crear un conjunto de datos compartido en Theano (sujeta a cambios de Pytorch)
+#   trainCoxMlp - función principal para entrenar el modelo cox-nnet
+#   predictNewData - función para predecir nuevos datos
+#   L2CVSearch - función auxiliar para realizar validación cruzada en un conjunto de entrenamiento, para seleccionar el parámetro de regularización L2 óptimo.
+#   CVLoglikelihood - calcula la probabilidad de validación cruzada (utilizando el método de Houwelingen et al. 2005)
+#   varImportance - calcula la importancia de las variables (utilizando el método de Fischer 2015)
+#   saveModel - guarda un modelo en un fichero: saveModel(model, file_name)
+#   loadModel - carga un modelo desde un fichero: loadModel(fileName)
