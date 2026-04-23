ef057510d0f87b44d7c86c85aaf200cc521f3231
MiguelAngel <mmontijanosempere@gmail.com>
2026-04-23 17:34:03 +0200

    Sexto commit: Clase CoxMLP en Pytorch finalizada

--- coxnnet_pytorch.py
+++ coxnnet_pytorch.py
@@ -1,8 +1,6 @@
 # Este es un programa para ejecutar la extensión de red neuronal de la regresión de Cox
-#Clases:
-#   CoxRegression - la capa de salida de la regresión de Cox
-#   HiddenLayer - capas ocultas entre la entrada y la salida
-#   CoxMlp - clase contenedora para la capa de salida y las capas ocultas
+#Clase:
+#   CoxMlp - clase contenedora para la capa de salida y la capa oculta
 
 #Funciones: 
 #   createSharedDataset - función auxiliar para crear un conjunto de datos compartido en Theano (sujeta a cambios de Pytorch)
@@ -24,19 +22,30 @@ from sklearn import model_selection as ms
 
 # Constante global para especificar si usamos la gpu como aceleradora o en el caso de que no haya una disponible usar cpu
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+# Para que los pesos sean los mismos siempre al empezar, siempre se hace globalmente
+torch.manual_seed(123)
 
 class CoxMLP(nn.Module):
     # Aquí se definen las capas de la NN
-    def __init__(self, n_input):
+    # Necesitaremos: tamaño de entrada/número de columnas y (opcional) número neuronas capa oculta
+    def __init__(self, n_input, n_hidden):
         super().__init__()
-        self.linear_tanh_stack = nn.Sequential(
-            nn.Linear(n_input, 512),
-            nn.Tanh,
-            nn.Linear(512, 1)
-        )
+
+        if n_hidden == None:
+            n_hidden = int(np.ceil(n_input ** 0.5))
+        
+        # Modo manual
+        self.input_layer = nn.Linear(n_input,n_hidden)
+        self.tanh = nn.Tanh()
+        self.output_layer = nn.Linear(n_hidden,1)
 
     # Y aquí se define como se avanza a través de la NN
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_tanh_stack(x)
-        return logits
+    def forward(self, input):
+
+        input_linear = self.input_layer(input)
+
+        linear_tanh = self.tanh(input_linear)
+
+        tanh_output = self.output_layer(linear_tanh)
+
+        return tanh_output
